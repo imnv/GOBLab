4f7c5893a8d9a56f10230820343bb91f9f6cbccb
Vesper <Nils.Vesper@gob.de>
2016-12-02 14:34:05 +0100

    Mein Toggle hinzugefügt

---
+++ GOB.Logging/Toggle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOB.Logging
+{
+    class Toggle
+    {
+        private static bool _toggle = false;
+
+        public static bool GetToggle
+        {
+            get
+            {
+                _toggle = !_toggle;
+                return !_toggle;
+            }
+        }
+    }
+}
